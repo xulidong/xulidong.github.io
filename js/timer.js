fabea24d4cd26354d6c5bad2e22f9851c538e251
xulidong <xu.lidong@qq.com>
2017-10-10 17:27:02 +0800

    init proj

---
+++ js/timer.js
@@ -0,0 +1,43 @@
+function tick() {
+	var hours, minutes, seconds, txtNow, txtDo;
+	var intHours, intMinutes, intSeconds;
+	var today;
+	today = new Date();
+	intHours = today.getHours();
+	intMinutes = today.getMinutes();
+	intSeconds = today.getSeconds();
+	if (intHours == 0) {
+		hours = "12:";
+		txtNow = "现在是午夜";
+		txtDo = "，该休息了。"
+	} else if (intHours < 12) {
+		hours = intHours + ":";
+		txtNow = "现在是上午";
+		txtDo = "，新的一天开始了。";
+	} else if (intHours == 12) {
+		hours = "12:";
+		txtNow = "现在是正午";
+		txtDo = "，该午休了。";
+	} else {
+		intHours = intHours - 12;
+		hours = intHours + ":";
+		txtNow = "现在是下午";
+		txtDo = "，抓紧时间吧。";
+	}
+
+	if (intMinutes < 10) {
+		minutes = "0" + intMinutes + ":";
+	} else {
+		minutes = intMinutes + ":";
+	}
+	if (intSeconds < 10) {
+		seconds = "0" + intSeconds;
+	} else {
+		seconds = intSeconds + "";
+	}
+	var timeString = "<div>" + txtNow + "<b>" + hours + minutes + seconds + "</b>" + txtDo + "</div>";
+	document.getElementById('clock').innerHTML = timeString;
+
+	window.setTimeout("tick();", 500);
+}
+window.onload = tick;
