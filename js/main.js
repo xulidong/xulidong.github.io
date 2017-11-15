0b8289fc2a6909c72c5c45681bed11693f57195c
xulidong <xulidong@macbook.local>
2017-11-15 10:40:16 +0800

    del  random theme

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 showCurTime();// 显示当前时间
 
-var rand = Math.random();
+// 暂时去掉随机显示主题
+// var rand = Math.random();
 
-if (rand < 0.2) {
-	showFireworks();
-} else if (rand < 0.4) {
-	showBubble();
-}else if (rand < 0.6) {
-	showRain();
-} else if (rand < 0.8) {
-	showSnow();
-} else {
-	// origin
-}
+// if (rand < 0.2) {
+// 	showFireworks();
+// } else if (rand < 0.4) {
+// 	showBubble();
+// }else if (rand < 0.6) {
+// 	showRain();
+// } else if (rand < 0.8) {
+// 	showSnow();
+// } else {
+// 	// origin
+// }
+
+showBubble();
